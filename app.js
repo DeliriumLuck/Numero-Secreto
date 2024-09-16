76a5f762fac7062dfe55bf0d2dc309d6f8a28a88
DeliriumLuck <richlopes10@gmail.com>
2024-09-16 17:55:35 -0300

    Apagando responsiveVoice

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
     console.log('Falando:', texto);
 }
 
